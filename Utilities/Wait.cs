d14294da2d836daf750cd74de39e7a0e3df5b6c4
Monali Marapana <monalimeg@gmail.com>
2021-10-08 23:22:56 +1300

    Introduce new custom Wait helper class

---
+++ Utilities/Wait.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Selenium_DOTNET_T1.Utilities
+{
+    class Wait
+    {
+        public void waitForElementToBeClickable(IWebDriver driver, string locaterType, string locaterValue, int seconds )
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(locaterValue)));
+
+        }
+    }
+}
