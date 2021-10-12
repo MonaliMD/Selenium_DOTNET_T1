5cdc6b550c25ce8332fd96d9d63b729f0c9cebc3
Monali Marapana <monalimeg@gmail.com>
2021-10-12 16:46:51 +1300

    Specflow configuration

---
+++ SpecDefinition/TMFeatureSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Selenium_DOTNET_T1.SpecDefinition
+{
+    [Binding]
+    public class TMFeatureSteps
+    {
+        [Given(@"I logged in to turnup prtal successfully")]
+        public void GivenILoggedInToTurnupPrtalSuccessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I navigate to time and material page")]
+        public void GivenINavigateToTimeAndMaterialPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I create time and material record")]
+        public void WhenICreateTimeAndMaterialRecord()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Then the record shoul be create successfully")]
+        public void ThenThenTheRecordShoulBeCreateSuccessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
